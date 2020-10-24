3ebf127fd63883ad351e2429c60e06a28bf62d58
joaonpsilva <joao.npsilva@ua.pt>
2020-10-24 18:57:18 +0100

    use pandas to chuck the file and process simpler and faster

--- Corpus.py
+++ Corpus.py
@@ -1,34 +1,18 @@
 import csv
+import pandas as pd
 
 class CorpusReader():
 
-    def __init__(self, csvfile):
-        self.csvfile = csvfile
-        self.file = open(self.csvfile, 'r')
-        self.reader = csv.reader(self.file, delimiter=',')
-        next(self.reader)   #Get rid of headers
-        self.doc = self.nextLine()    #Store next value to return so that can check before if exists
-    
-    def hasNext(self):
-        return not self.doc is None
-    
-    def nextDocument(self):
-        temp = self.doc
-        self.doc = self.nextLine()
-        return temp
- 
-    def nextLine(self):     #Read line of csv
+    def __init__(self, csvfile, chunkSize=100):
+        self.fileIterator = pd.read_csv(csvfile, chunksize=chunkSize, iterator=True)
 
-        while True:
+    def getNextChunk(self):
+        try:
+            chunk = self.fileIterator.get_chunk()
+        except StopIteration:
+            return None
 
-            try:
-                line = next(self.reader)
-            except StopIteration:           #End of file
-                return None
-            
-            hashid, title, abstract = line[0],line[2],line[7]
+        chunk = chunk.dropna(subset=['abstract'])
+        chunk = chunk[['doi', 'title', 'abstract']]
 
-            if hashid == '' or abstract == '':       #ignore documents with empty hash and abstarct 
-                continue
-
-            return hashid, title, abstract 
+        return chunk.values
