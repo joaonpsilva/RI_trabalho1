e140bfb69382467edce9d06c276caca9dbd568f2
bernasrodrigues <47544028+bernasrodrigues@users.noreply.github.com>
2020-10-26 20:46:44 +0000

    Add files via upload

--- Tokenizer2.py
+++ Tokenizer2.py
@@ -1,3 +1,4 @@
+import re
 import Stemmer
 
 
@@ -6,6 +7,8 @@ class Tokenizer2:
     def __init__(self, stopwordsfile):
         self.stemmer = Stemmer.Stemmer("english")
         self.stopwords = self.buildStopWords(stopwordsfile)
+        self.specialCases = ["@", "-"]  # Caracters to be specially processed
+        self.regex = re.compile('[^a-zA-Z0-9@\'-]')
 
     def buildStopWords(self, file):
         reader = open(file, 'r')
@@ -14,8 +17,23 @@ class Tokenizer2:
     def removeStopWords(self, words):
         return [word for word in words if not word in self.stopwords]
 
-    def process(self, words):
-        words = self.stemmer.stemWords(words)
-        words = self.removeStopWords(words)
+    def matchSpecialCases(self, words):  # Not used since the stemmer already handles the special cases
+        return [s for s in words if any(xs in s for xs in self.specialCases)]
 
-        return words
+    def process(self, *phrases):
+        terms = []
+
+        for phrase in phrases:
+            a = phrase
+            phrase = self.regex.sub(' ', phrase)
+            phrase = phrase.lower()
+            words = phrase.split(' ')
+
+            words = [word.strip('\'-') for word in words if len(word.strip('\'-')) > 2]  # replace small words and remove apostrofes and hifens in the begining and end
+            words = self.stemmer.stemWords(words)
+            words = self.removeStopWords(words)
+
+
+            terms += words
+
+        return terms
