5b7b0c4f707f66f56b5bb1e3402449d2b377b3e3
joaonpsilva <joao.npsilva@ua.pt>
2020-10-27 21:03:57 +0000

    delete not needed features for now

--- Indexer.py
+++ Indexer.py
@@ -3,7 +3,6 @@ from Tokenizer1 import Tokenizer1
 from Tokenizer2 import Tokenizer2
 from Posting import Posting
 import time
-import sys
 import heapq
 import argparse
 import os
@@ -53,7 +52,6 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("-tokenizer", type=int, choices=[1,2], required=True, help="tokenizer")
     parser.add_argument("-f", type=str, default="all_sources_metadata_2020-03-13.csv", help="text")
-    parser.add_argument("-user", type=bool , default=False , help="Opção de menu de pesquisa de termos")
     args = parser.parse_args()
 
     corpusreader = CorpusReader(args.f)
@@ -69,7 +67,6 @@ if __name__ == "__main__":
     t2 = time.time()
 
     print('seconds: ', t2-t1)
-    print('Indexer memory               : ', sys.getsizeof(indexer.invertedIndex))
     print("Total memory used by programm: ", process.memory_info().rss)
     
     keyList = list(indexer.invertedIndex.keys())
@@ -80,15 +77,3 @@ if __name__ == "__main__":
     
     mostUsed = heapq.nlargest(10, indexer.invertedIndex.items(), key=lambda item: len(item[1]))
     print("Higher doc freq: ", [(i[0], len(i[1])) for i in mostUsed])
-
-    if args.user:
-        while True:
-            termo = input("Introduza o termo a pesquisar: ")
-            start_time = time.time()
-            termo = termo.lower()
-            if termo in indexer.invertedIndex.keys():
-                print(indexer.invertedIndex[termo])
-            else:
-                print("Termo não existente")
-                print("Keys parecidas: " , [key for key in indexer.invertedIndex.keys() if termo in key and len(termo) > 0])
-            print("---Pesquisa feita em %s seconds ---" % (time.time() - start_time))
