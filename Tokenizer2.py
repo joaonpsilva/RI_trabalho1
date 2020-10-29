eca990ec1c5ba30982ac09cd8387f5f03d914363
joaonpsilva <joao.npsilva@ua.pt>
2020-10-29 15:39:46 +0000

    stopWords into set (much faster)

--- Tokenizer2.py
+++ Tokenizer2.py
@@ -1,38 +1,35 @@
+
 import re
 import Stemmer
 
 
 class Tokenizer2:
 
-    def __init__(self, stopwordsfile):
+    def __init__(self, stopwordsfile='snowball_stopwords_EN.txt'):
         self.stemmer = Stemmer.Stemmer("english")
         self.stopwords = self.buildStopWords(stopwordsfile)
-        self.specialCases = ["@", "-"]  # Caracters to be specially processed
-        self.regex = re.compile('[^a-zA-Z0-9@\'-]')
+        self.regex = re.compile('[^a-zA-Z0-9@\.\' -]')
 
     def buildStopWords(self, file):
         reader = open(file, 'r')
-        return reader.read().splitlines()
+        return set(reader.read().splitlines())
 
     def removeStopWords(self, words):
         return [word for word in words if not word in self.stopwords]
-
-    def matchSpecialCases(self, words):  # Not used since the stemmer already handles the special cases
-        return [s for s in words if any(xs in s for xs in self.specialCases)]
 
     def process(self, *phrases):
         terms = []
 
         for phrase in phrases:
-            a = phrase
-            phrase = self.regex.sub(' ', phrase)
+ 
+            phrase = self.regex.sub('', phrase)
             phrase = phrase.lower()
             words = phrase.split(' ')
 
-            words = [word.strip('\'-') for word in words if len(word.strip('\'-')) > 2]  # replace small words and remove apostrofes and hifens in the begining and end
-            words = self.stemmer.stemWords(words)
+            words = [word.strip('.\'-') for word in words if len(word.strip('.\'-')) > 2]  # removes small words, https part and remove apostrofes and hifens in the begining and end
+            
             words = self.removeStopWords(words)
-
+            words = self.stemmer.stemWords(words)
 
             terms += words
 
